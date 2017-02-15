d3235846cfd30d53974c133a03d655ed06304554
Khayyam Saleem <khayyamsaleemgmail.com@>
2017-02-15 11:20:56 -0500

    set up datalab hw

---
+++ include/bits.h
@@ -0,0 +1,16 @@
+
+int bitAnd(int, int);
+int bitNor(int, int);
+int copyLSB(int);
+int isEqual(int, int);
+int bitMask(int, int);
+int bitCount(int);
+int tmax();
+int isNonNegative(int);
+int addOK(int, int);
+int rempwr2(int, int);
+int isLess(int, int);
+int absVal(int);
+int isPower2(int);
+unsigned float_neg(unsigned);
+unsigned float_abs(unsigned);
