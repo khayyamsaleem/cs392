cbbfc45c2a5bcb6b27751425bdb1130a20f072db
Khayyam Saleem <khayyamsaleemgmail.com@>
2017-02-17 20:18:54 -0500

    finished, untested

--- src/bits/bits.c
+++ src/bits/bits.c
@@ -1,17 +1,18 @@
-/* 
- * CS 392 Systems Programming - Homework 1 Data Lab 
- * 
- * Name - 
- * Email Id - 
- * Date - 
+/*
+ * CS 392 Systems Programming - Homework 1 Data Lab
  *
- * Stevens Honour Code - 
- * 
+ * Name - Khayyam Saleem
+ * Email Id - ksaleem
+ * Date - 02/17/2017
+ *
+ * Stevens Honour Code - I pledge my honor that I have
+ * abided by the Stevens Honor System.
+ *
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
  *
  * WARNING: Do not include the <stdio.h> header; this is for grading purposes.
- * You can use the same using another control file with main.  
+ * You can use the same using another control file with main.
  */
 
 #if 0
@@ -25,119 +26,118 @@ You will provide your solution to the Data Lab by
 editing the collection of functions in this source file.
 
 INTEGER CODING RULES:
- 
-  Replace the "return" statement in each function with one
-  or more lines of C code that implements the function. Your code 
-  must conform to the following style:
- 
-  int Funct(arg1, arg2, ...) {
-      /* brief description of how your implementation works */
-      int var1 = Expr1;
-      ...
-      int varM = ExprM;
 
-      varJ = ExprJ;
-      ...
-      varN = ExprN;
-      return ExprR;
-  }
+Replace the "return" statement in each function with one
+or more lines of C code that implements the function. Your code
+must conform to the following style:
 
-  Each "Expr" is an expression using ONLY the following:
-  1. Integer constants 0 through 255 (0xFF), inclusive. You are
-      not allowed to use big constants such as 0xffffffff.
-  2. Function arguments and local variables (no global variables).
-  3. Unary integer operations ! ~
-  4. Binary integer operations & ^ | + << >>
-    
-  Some of the problems restrict the set of allowed operators even further.
-  Each "Expr" may consist of multiple operators. You are not restricted to
-  one operator per line.
+int Funct(arg1, arg2, ...) {
+    /* brief description of how your implementation works */
+    int var1 = Expr1;
+    ...
+        int varM = ExprM;
 
-  You are expressly forbidden to:
-  1. Use any control constructs such as if, do, while, for, switch, etc.
-  2. Define or use any macros.
-  3. Define any additional functions in this file.
-  4. Call any functions.
-  5. Use any other operations, such as &&, ||, -, or ?:
-  6. Use any form of casting.
-  7. Use any data type other than int.  This implies that you
-     cannot use arrays, structs, or unions.
+    varJ = ExprJ;
+    ...
+        varN = ExprN;
+    return ExprR;
+}
 
- 
-  You may assume that your machine:
-  1. Uses 2s complement, 32-bit representations of integers.
-  2. Performs right shifts arithmetically.
-  3. Has unpredictable behavior when shifting an integer by more
-     than the word size.
+Each "Expr" is an expression using ONLY the following:
+1. Integer constants 0 through 255 (0xFF), inclusive. You are
+not allowed to use big constants such as 0xffffffff.
+2. Function arguments and local variables (no global variables).
+3. Unary integer operations ! ~
+4. Binary integer operations & ^ | + << >>
+
+Some of the problems restrict the set of allowed operators even further.
+Each "Expr" may consist of multiple operators. You are not restricted to
+one operator per line.
+
+You are expressly forbidden to:
+1. Use any control constructs such as if, do, while, for, switch, etc.
+2. Define or use any macros.
+3. Define any additional functions in this file.
+4. Call any functions.
+5. Use any other operations, such as &&, ||, -, or ?:
+6. Use any form of casting.
+7. Use any data type other than int.  This implies that you
+cannot use arrays, structs, or unions.
+
+
+You may assume that your machine:
+1. Uses 2s complement, 32-bit representations of integers.
+2. Performs right shifts arithmetically.
+3. Has unpredictable behavior when shifting an integer by more
+than the word size.
 
 EXAMPLES OF ACCEPTABLE CODING STYLE:
-  /*
-   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
-   */
-  int pow2plus1(int x) {
-     /* exploit ability of shifts to compute powers of 2 */
-     return (1 << x) + 1;
-  }
+/*
+ * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+ */
+int pow2plus1(int x) {
+    /* exploit ability of shifts to compute powers of 2 */
+    return (1 << x) + 1;
+}
 
-  /*
-   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
-   */
-  int pow2plus4(int x) {
-     /* exploit ability of shifts to compute powers of 2 */
-     int result = (1 << x);
-     result += 4;
-     return result;
-  }
+/*
+ * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+ */
+int pow2plus4(int x) {
+    /* exploit ability of shifts to compute powers of 2 */
+    int result = (1 << x);
+    result += 4;
+    return result;
+}
 
 FLOATING POINT CODING RULES
 
 For the problems that require you to implent floating-point operations,
-the coding rules are less strict.  You are allowed to use looping and
-conditional control.  You are allowed to use both ints and unsigneds.
-You can use arbitrary integer and unsigned constants.
+    the coding rules are less strict.  You are allowed to use looping and
+    conditional control.  You are allowed to use both ints and unsigneds.
+    You can use arbitrary integer and unsigned constants.
 
-You are expressly forbidden to:
-  1. Define or use any macros.
-  2. Define any additional functions in this file.
-  3. Call any functions.
-  4. Use any form of casting.
-  5. Use any data type other than int or unsigned.  This means that you
-     cannot use arrays, structs, or unions.
-  6. Use any floating point data types, operations, or constants.
+    You are expressly forbidden to:
+    1. Define or use any macros.
+    2. Define any additional functions in this file.
+    3. Call any functions.
+    4. Use any form of casting.
+    5. Use any data type other than int or unsigned.  This means that you
+    cannot use arrays, structs, or unions.
+    6. Use any floating point data types, operations, or constants.
 
 
-NOTES:
-  1. Each function has a maximum number of operators (! ~ & ^ | + << >>)
-     that you are allowed to use for your implementation of the function. 
-     The max operator count is checked by the TAs. Note that '=' is not 
-     counted; you may use as many of these as you want without penalty.
-  2. Make sure you test for all cases, especially the edge ones.
-  3. The maximum number of ops for each function is given in the
-     header comment for each function. If there are any inconsistencies 
-     between the maximum ops in the writeup and in this file, consider
-     this file the authoritative source.
+    NOTES:
+1. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+    that you are allowed to use for your implementation of the function.
+    The max operator count is checked by the TAs. Note that '=' is not
+    counted; you may use as many of these as you want without penalty.
+    2. Make sure you test for all cases, especially the edge ones.
+    3. The maximum number of ops for each function is given in the
+    header comment for each function. If there are any inconsistencies
+    between the maximum ops in the writeup and in this file, consider
+    this file the authoritative source.
 
-/*
- * Your code will be checked for C coding rules and standards.
- *  
- */
+    /*
+     * Your code will be checked for C coding rules and standards.
+     *
+     */
 
 
 #endif
-/* 
- * bitAnd - x&y using only ~ and | 
- *   Example: bitAnd(6, 5) = 4
- *   Legal ops: ~ |
- *   Max ops: 8
- *   Rating: 1
- *   Solution Provided for reference
- */
-int bitAnd(int x, int y) {
-
-  return ~(~x | ~y);
-}
-/* 
- * bitNor - ~(x|y) using only ~ and & 
+    /*
+     * bitAnd - x&y using only ~ and |
+     *   Example: bitAnd(6, 5) = 4
+     *   Legal ops: ~ |
+     *   Max ops: 8
+     *   Rating: 1
+     *   Solution Provided for reference
+     */
+    int bitAnd(int x, int y) {
+        return ~(~x | ~y);
+    }
+/*
+ * bitNor - ~(x|y) using only ~ and &
  *   Example: bitNor(0x6, 0x5) = 0xFFFFFFF8
  *   Legal ops: ~ &
  *   Max ops: 8
@@ -146,7 +146,7 @@ int bitAnd(int x, int y) {
 int bitNor(int x, int y) {
     return  (~x) & (~y);
 }
-/* 
+/*
  * copyLSB - set all bits of result to least significant bit of x
  *   Example: copyLSB(5) = 0xFFFFFFFF, copyLSB(6) = 0x00000000
  *   Legal ops: ! ~ & ^ | + << >>
@@ -157,17 +157,17 @@ int copyLSB(int x) {
     return (x << 31) >> 31;
 }
 /*
- * isEqual - return 1 if x == y, and 0 otherwise 
+ * isEqual - return 1 if x == y, and 0 otherwise
  *   Examples: isEqual(5,5) = 1, isEqual(4,5) = 0
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 5
  *   Rating: 2
  */
 int isEqual(int x, int y) {
-  return !(x ^ y);
+    return !(x ^ y);
 }
-/* 
- * bitMask - Generate a mask consisting of all 1's 
+/*
+ * bitMask - Generate a mask consisting of all 1's
  *   lowbit and highbit
  *   Examples: bitMask(5,3) = 0x38
  *   Assume 0 <= lowbit <= 31, and 0 <= highbit <= 31
@@ -177,7 +177,10 @@ int isEqual(int x, int y) {
  *   Rating: 3
  */
 int bitMask(int highbit, int lowbit) {
-  return 2;
+    int lo = ~0 << lowbit;
+    int hi = ~0 << highbit;
+    hi = ~(hi << 1);
+    return lo & hi;
 }
 /*
  * bitCount - returns count of number of 1's in word
@@ -187,19 +190,44 @@ int bitMask(int highbit, int lowbit) {
  *   Rating: 4
  */
 int bitCount(int x) {
-  return 2;
+    int m1, m2;
+
+    // encompasses two ls bytes
+    m1 = 0x11 | (0x11 << 8);
+
+    // encompasses final bytes
+    m2 = m1 | (m1 << 16);
+
+    //sum holds # of 1's in string
+    int sum = x & m2;
+    sum = sum + ((x >> 1) & m2);
+    sum = sum + ((x >> 2) & m2);
+    sum = sum + ((x >> 3) & m2);
+
+    //sum has # of 1's in first 4 bits
+    // strip away 1's after first 4 bits
+    sum = sum + (sum >> 16);
+
+    //preserve sum, mask 1's in byte 2
+    m1 = 0xF | (0xF << 8);
+
+    //toggle bits in sum, adds toggled 4 bits
+    sum = (sum & m1) + ((sum >> 4) & m1);
+
+    //shift sum by byte, mask sum so 6 bits remain
+    return ((sum + (sum >> 8)) & 0x3F);
 }
-/* 
- * TMax - return maximum two's complement integer 
+/*
+ * TMax - return maximum two's complement integer
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 4
  *   Rating: 1
  */
 int tmax(void) {
-  return 2;
+    return ~(1<<31);
 }
-/* 
- * isNonNegative - return 1 if x >= 0, return 0 otherwise 
+/*
+ * isNonNegative - return 1 if x >= 0, return 0 otherwise
  *   Example: isNonNegative(-1) = 0.  isNonNegative(0) = 1.
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 6
@@ -208,18 +236,24 @@ int tmax(void) {
 int isNonNegative(int x) {
     return !(x>>31);
 }
-/* 
+/*
  * addOK - Determine if can compute x+y without overflow
  *   Example: addOK(0x80000000,0x80000000) = 0,
- *            addOK(0x80000000,0x70000000) = 1, 
+ *            addOK(0x80000000,0x70000000) = 1,
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 20
  *   Rating: 3
  */
 int addOK(int x, int y) {
-  return 2;
+    int sum = x + y;
+    int xsign = x >> 31;
+    int ysign = y >> 31;
+    int xysign = sum >> 31;
+    //overflows when sign of x and y are the same,
+    //but sign of sum differs
+    return !(~(xsign ^ ysign) & (xsign ^ xysign));
 }
-/* 
+/*
  * rempwr2 - Compute x%(2^n), for 0 <= n <= 30
  *   Negative arguments should yield negative remainders
  *   Examples: rempwr2(15,2) = 3, rempwr2(-35,3) = -3
@@ -228,19 +262,24 @@ int addOK(int x, int y) {
  *   Rating: 3
  */
 int rempwr2(int x, int n) {
-  return 2;
+    int m = x>>31;
+    return ((((x^m)+(1&m))&(~(((~0)>>31)<<n)))^m)+(1&m);
 }
-/* 
- * isLessOrEqual - if x <= y  then return 1, else return 0 
+/*
+ * isLessOrEqual - if x <= y  then return 1, else return 0
  *   Example: isLessOrEqual(4,5) = 1.
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 24
  *   Rating: 3
  */
 int isLess(int x, int y) {
-  return 2;
+    int opp_sign = ((x^y) >> 31);
+    int neg_x = x >> 31;
+    int sub = (~((y + (~x + 1)) >> 31)); //not(sgn(y-x)) over 32 bits
+
+    return (!!(neg_x&opp_sign)) | (!!(sub&(~opp_sign)));
 }
-/* 
+/*
  * absVal - absolute value of x
  *   Example: absVal(-1) = 1.
  *   You may assume -TMax <= x <= TMax
@@ -249,7 +288,8 @@ int isLess(int x, int y) {
  *   Rating: 4
  */
 int absVal(int x) {
-  return 2;
+    int y = x << 31;
+    return (~y & x) | (y & (~x+1));
 }
 /*
  * isPower2 - returns 1 if x is a power of 2, and 0 otherwise
@@ -260,9 +300,11 @@ int absVal(int x) {
  *   Rating: 4
  */
 int isPower2(int x) {
- return 2;
+    //check if x&(x-1) == 0, exclude neg and 0.
+    int y = x + ~0;
+    return !((x&y)|((x >> 31) | !x));
 }
-/* 
+/*
  * float_neg - Return bit-level equivalent of expression -f for
  *   floating point argument f.
  *   Both the argument and result are passed as unsigned int's, but
@@ -274,9 +316,17 @@ int isPower2(int x) {
  *   Rating: 2
  */
 unsigned float_neg(unsigned uf) {
-  return 2;
+    unsigned checkNaN = 0x000000FF << 23; //1's
+    int fpoint = 0x7FFFFF&uf; //fractional portion
+
+    //return arg if exponent bits at powers of 2
+    //are q's and fraction is not zero
+    if((checkNaN & uf) == checkNaN && fpoint)
+        return uf;
+    //else, flip sign bit
+    return uf ^ (1 << 31);
 }
-/* 
+/*
  * float_abs - Return bit-level equivalent of absolute value of f for
  *   floating point argument f.
  *   Both the argument and result are passed as unsigned int's, but
@@ -288,5 +338,14 @@ unsigned float_neg(unsigned uf) {
  *   Rating: 2
  */
 unsigned float_abs(unsigned uf) {
-  return 2;
+    unsigned m = 0x7FFFFFFF;
+    unsigned mNaN = 0x7F800001;
+    unsigned result = uf & m; //sets sign bit to 0
+
+    //if NaN, return arg. all NaN's are greater than
+    //mNaN.
+    if (result >= mNaN)
+        return uf;
+    else
+        return result;
 }
