17e691e52d9d3e2754ee69af33fb275a7df52317
Khayyam Saleem <khayyamsaleemgmail.com@>
2017-02-28 21:14:50 -0500

    made my_strcat c90 compliant

--- src/my/my_strcat.c
+++ src/my/my_strcat.c
@@ -4,10 +4,12 @@
  * copies src onto end of dst and returns dst
  */
 char *my_strcat(char *dst, char *src){
+    char *concat;
+
     if(dst == NULL)
         return NULL;
 
-    char *concat = dst;
+    concat = dst;
 
     while(*concat != '\0')
         concat++;
