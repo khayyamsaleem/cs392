53af38668cd5b1f84e790b1428dea69f60754c28
Khayyam Saleem <khayyam.saleem@gmail.com>
2017-03-07 14:44:36 -0500

    modified strnconcat tests

--- test/testmy.c
+++ test/testmy.c
@@ -88,7 +88,8 @@ int main(){
     my_char('\n');
     my_str("Testing my_atoi, ('a-b-54sc85-ed') -> 54\n");
     my_int(my_atoi("a-b-54sc85-ed"));
-    printf("%s", my_strnconcat(NULL, NULL, 5));
+    my_char('\n');
+    printf("%s\n", my_strnconcat(NULL, NULL, 5));
     my_char('\n');
     return 0;
 
