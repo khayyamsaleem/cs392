2f07c15dec60c69edf91852448623e77e09b6e5d
Khayyam Saleem <khayyam.saleem@gmail.com>
2017-05-16 10:03:00 -0400

    sockettalk

--- src/sockettalk/client.c
+++ src/sockettalk/client.c
@@ -1,74 +1,382 @@
 #include "my.h"
-#include <errno.h>
-#include <netdb.h>
-#include <poll.h>
-#include <signal.h>
+#include "list.h"
+#include <unistd.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <strings.h>
+#include <string.h>
+#include <ncurses.h>
+#include <term.h>
 
-//0 if stopped, 1 if running
-int status;
+int main(int argc, char* argv[]) {
+    int sockfd, n;
+    struct sockaddr_in server;
+    struct hostent *hp;
+    char username[100];
+    char bufferi[1024];
+    char buffero[1124];
+    char* exit_sequence = "garbage";
+    fd_set readfds;
+    int x = 1;
+    int y = 0;
+    int last_char = 0;
+    int char_pos = 0;
+    int ch;
+    int new_line;
+    int line_jump;
+    int y_diff;
+    int last_char_x;
 
-char *get_input(){
-    char *buffer = calloc(1, 1024 + 1);
-    int s = read(0, buffer, 1024);
-
-    //reached end of input
-    if (s == 0) {
-        free(buffer);
-        return NULL;
-    }
-    
-    if (s < 0){ //error case
-        perror("error while reading input.");
-        free(buffer);
-        return NULL;
-    }
-
-    if (buffer[s - 1] == '\n'){
-        buffer[s-1] = '\0'; //terminate input
-    }
-    buffer[s] = '\0';
-
-    return buffer;
-}
-
-char *get_sock(int fd){
-    char *buffer = calloc(1, 1024 + 1);
-    int s = recv(fd, buffer, 1024, 0);
-
-    //error cases
-    if (s == 0) {
-        my_str("The server has disconnected.\n");
-        free(buffer);
-        return NULL;
+    if (argc != 3) {
+        my_str("Usage: ./client <host> <port>");
+        exit(0);
     }
 
-    if (s < 0){
-        perror("Receipt error");
-        free(buffer);
-        return NULL;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        my_str("Socket Error\n");
+        exit(0);
+    }
+    hp = gethostbyname(argv[1]);
+    if (hp == 0) {
+        my_str("Host Error: host does not exist\n");
+        exit(0);
+    }
+    bzero((char *) &server, sizeof(server));
+    server.sin_family = AF_INET;
+    bcopy((char *)hp->h_addr_list[0], (char *)&server.sin_addr.s_addr, hp->h_length);
+    server.sin_port = htons(atoi(argv[2]));
+
+    initscr();
+    keypad(stdscr, TRUE);
+    scrollok(stdscr, TRUE);
+    erase();
+    clear();
+    noecho();
+    start_color();
+    if ( has_colors() && COLOR_PAIRS >= 2) {
+        init_pair(1, COLOR_MAGENTA, COLOR_BLACK);
+        init_pair(2, COLOR_GREEN, COLOR_BLACK);
     }
 
-    //null-terminate
-    buffer[s] = '\0';
-    return buffer;
-}
-
-void kill_client_conn(){
-    status = 0;
-}
-
-int main(int argc, char *argv[]){
-    if (argc != 3){
-        my_str("Usage: ./client <host> <port>\n");
-        return 1;
+    x = 25;
+    color_set(2, NULL);
+    printw("Please enter a username: ");
+    move(y, x);
+    while( (ch = getch()) != 10) {
+        color_set(1, NULL);
+        if (ch == KEY_RESIZE) {
+            refresh();
+        }
+        else if (ch == KEY_BACKSPACE) {
+            if ((y == 0 && x > 25) || y > 0) {
+                if (char_pos > 0) {
+                    char_pos--;
+                    last_char--;
+                }
+                if (char_pos == last_char)
+                    username[char_pos] = '\0';
+                else {
+                    for (int i = char_pos; i <= last_char; i++) {
+                        username[i] = username[i+1];
+                    }
+                }
+                move(0, 25);
+                clrtobot();
+                addstr(username);
+                if (x > 0) {
+                    x--;
+                }
+                else {
+                    x = COLS - 1;
+                    y--;
+                }
+                move(y, x);
+            }
+        }
+        else if (ch == KEY_DC) {
+            if (char_pos < last_char) {
+                for (int i = char_pos; i <= last_char; i++) {
+                    username[i] = username[i + 1];
+                }
+                move(0, 25);
+                clrtobot();
+                addstr(username);
+                last_char--;
+                move(y, x);
+            }
+        }
+        else if (ch == KEY_LEFT) {
+            if ((y == 0 && x > 25) || y > 0) {
+                if (x > 0) {
+                    x--;
+                }
+                else {
+                    x = COLS - 1;
+                    y--;
+                }
+                move(y, x);
+                char_pos--;
+            }
+        }
+        else if (ch == KEY_RIGHT) {
+            if (char_pos < last_char) {
+                if (x < COLS - 1) {
+                    x++;
+                }
+                else {
+                    x = 0;
+                    y++;
+                }
+                move(y, x);
+                char_pos++;
+            }
+        }
+        else if (ch >= 32 && ch <= 126){
+            if (last_char < 99) {
+                if (char_pos != last_char) {
+                    for (int i = last_char + 1; i > char_pos; i--) {
+                        username[i] = username[i - 1];
+                    }
+                    username[char_pos] = (char)ch;
+                    move(0, 25);
+                    clrtobot();
+                    addstr(username);
+                }
+                else {
+                    username[last_char] = (char)ch;
+                    username[last_char + 1] = '\0';
+                    addch(ch);
+                }
+                char_pos++;
+                last_char++;
+                if (x < COLS - 1) {
+                    x++;
+                }
+                else {
+                    x = 0;
+                    y++;
+                }
+                move(y, x);
+            }
+        }
     }
 
-    //store arguments
-    char *host = argv[1];
-    int p = my_atoi(argv[2]);
+    last_char = 0;
+    char_pos = 0;
+    x = 0;
+    y++;
+    move(y, x);
+    new_line = y;
 
+    if (connect(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        endwin();
+        my_str("Connection error\n");
+        exit(0);
+    }
 
+    if ((n = write(sockfd, username, 100)) < 0) {
+        endwin();
+        my_str("Write Error: error writing to socket\n");
+        exit(0);
+    }
 
-    
+    nodelay(stdscr, TRUE);
+    bzero(bufferi, 1024);
+
+    while(1) {
+        FD_ZERO(&readfds);
+        FD_SET(0, &readfds);
+        FD_SET(sockfd, &readfds);
+        color_set(1, NULL);
+
+        select(sockfd+1, &readfds, NULL, NULL, NULL);
+        if(FD_ISSET(sockfd, &readfds)) {
+            bzero(buffero, 1124);
+            color_set(2, NULL);
+
+            if ((n = read(sockfd,buffero,1124)) <= 0) {
+                printw("Server closed. Exiting...");
+                endwin();
+                exit(0);
+            }
+            if (my_strcmp(exit_sequence, &buffero[0]) == 0) {
+                printw("Exit Successful");
+                endwin();
+                exit(0);
+            }
+            move(new_line, 0);
+            clrtobot();
+            y_diff = y - new_line;
+            addstr(buffero);
+            line_jump = (my_strlen(buffero) / COLS) + 1;
+            if (y < LINES - line_jump) {
+                new_line += line_jump;
+                y = new_line;
+            }
+            else {
+                new_line = LINES - 1;
+                y = LINES - 1;
+                printw("\n");
+            }
+            move(y, 0);
+            if (my_strlen(bufferi) > 0) {
+                line_jump = (my_strlen(bufferi) / COLS) + 1;
+                color_set(1, NULL);
+                addstr(bufferi);
+                if (y < LINES - line_jump) {
+                    new_line = y;
+                    y = new_line + y_diff;
+                }
+                else {
+                    new_line = LINES - line_jump;
+                    y = new_line + y_diff;
+                }
+                move(y, x);
+            }
+            else {
+                x = 0;
+            }
+        }
+
+        ch = getch();
+        if (ch == ERR) {}
+        else if (ch == 10) {
+            if ((n = write(sockfd, bufferi, 1024)) < 0) {
+                printw("Write error.");
+                endwin();
+                exit(0);
+            }
+            bzero(bufferi, 1024);
+            move(new_line, 0);
+            clrtobot();
+            char_pos = 0;
+            last_char = 0;
+            x = 0;
+        }
+        else if (ch == KEY_RESIZE) {
+            int a, b;
+            getmaxyx(stdscr, a, b);
+            wresize(stdscr, a, b);
+        }
+        else if (ch == KEY_BACKSPACE) {
+            if (x > 0 || y != new_line) {
+                if (char_pos > 0) {
+                    char_pos--;
+                    last_char--;
+                }
+                if (char_pos == last_char)
+                    bufferi[char_pos] = '\0';
+                else {
+                    for (int i = char_pos; i <= last_char; i++) {
+                        bufferi[i] = bufferi[i+1];
+                    }
+                }
+                move(new_line, 0);
+                clrtobot();
+                addstr(bufferi);
+                if (x > 0) {
+                    x--;
+                }
+                else {
+                    x = COLS - 1;
+                    y--;
+                }
+                move(y, x);
+            }
+        }
+        else if (ch == KEY_DC) {
+            if (char_pos < last_char) {
+                for (int i = char_pos; i <= last_char; i++) {
+                    bufferi[i] = bufferi[i + 1];
+                }
+                move(new_line, 0);
+                clrtobot();
+                addstr(bufferi);
+                last_char--;
+                move(y, x);
+            }
+        }
+        else if (ch == KEY_LEFT) {
+            if (x > 0 || y != new_line) {
+                if (x > 0) {
+                    x--;
+                }
+                else {
+                    x = COLS - 1;
+                    y--;
+                }
+                move(y, x);
+                char_pos--;
+            }
+        }
+        else if (ch == KEY_RIGHT) {
+            if (char_pos < last_char) {
+                if (x < COLS - 1) {
+                    x++;
+                }
+                else {
+                    x = 0;
+                    y++;
+                }
+                move(y, x);
+                char_pos++;
+            }
+        }
+        else if (ch >= 32 && ch <= 126){
+            if (last_char < 1023) {
+                if (char_pos != last_char) {
+                    for (int i = last_char + 1; i > char_pos; i--) {
+                        bufferi[i] = bufferi[i - 1];
+                    }
+                    bufferi[char_pos] = (char)ch;
+                    line_jump = ((my_strlen(bufferi) - 1) / COLS) + 1;
+                    y_diff = y - new_line;
+                    last_char_x = last_char - (COLS * (line_jump - 1));
+                    move(new_line, 0);
+                    if (y >= LINES - line_jump && last_char_x == COLS - 1 && last_char > 0) {
+                        new_line--;
+                        y--;
+                    }
+                    clrtobot();
+                    addstr(bufferi);
+                    if (x < COLS - 1) {
+                        x++;
+                    }
+                    else {
+                        x = 0;
+                        y++;
+                    }
+                }
+                else {
+                    bufferi[last_char] = (char)ch;
+                    bufferi[last_char + 1] = '\0';
+                    addch(ch);
+                    if (x < COLS - 1) {
+                        x++;
+                    }
+                    else {
+                        x = 0;
+                        if (y < LINES - 1) {
+                            y++;
+                        }
+                        else {
+                            new_line--;
+                        }
+                    }
+                }
+                char_pos++;
+                last_char++;
+                move(y, x);
+            }
+        }
+        refresh();
+    }
+    close(sockfd);
+    endwin();
+    return 0;
 }
---
+++ src/sockettalk/server.c
@@ -0,0 +1,328 @@
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <strings.h>
+#include "my.h"
+#include "list.h"
+
+
+//struct to define client
+struct client {
+    int fd;
+    char* usr;
+    struct hostent *h;
+    char *hostaddr;
+};
+
+//client constructor
+struct client* user(int fd, char* usr, struct hostent* h, char* hostaddr){
+    struct client* cli = (struct client*)malloc(sizeof(struct client));
+    cli->fd = fd;
+    cli->usr = usr;
+    cli->h = h;
+    cli->hostaddr = hostaddr;
+    return cli;
+}
+
+int spec_options(char msg[512]){
+    char *cmd  = (char *)malloc(sizeof(char) * 7);
+    int i = 0; //maintains current char index
+    for(i = 0; i < 4; i++){
+        cmd[i] = msg[i];
+    }
+    if(my_strcmp(cmd, "/me ") == 0){
+        return 1;
+    } else {
+        //advance to length of longest possible special cmd
+        cmd[i] = msg[i];
+        cmd[i+1] = msg[i+1];
+        if(my_strcmp(cmd, "/nick ") == 0){
+            return 2;
+        } else if (my_strcmp(cmd, "/exit") == 0 || my_strcmp(cmd, "/exit ") == 0){
+            return 3;
+        } else {
+            return -1;
+        }
+    }
+}
+
+char* get_usr(char buf[1024]){
+    int x = my_strlen(buf);
+    char *usr = (char*)malloc(sizeof(char) * (x+1)); //leaving space for null terminator
+    int i = 0;
+    for (i = 0; i < x; i++){
+        usr[i] = buf[i];
+    }
+    usr[i] = '\0';
+    return usr;
+}
+
+//commands
+
+
+int main(int argc, char* argv[]){
+    struct sockaddr_in server;
+    struct sockaddr_in cli;
+    char buf[100];
+    char msg[1024];
+    int options;
+    int n;
+    int port;
+    socklen_t cli_len;
+    int cfd;
+    int pfd;
+    struct hostent *h;
+    char *hostaddr;
+    int c = 0;
+
+    if(argc != 2){
+        my_str("Usage: ./server <port>\n");
+        return 1;
+    }
+
+    port = my_atoi(argv[1]);
+    pfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (pfd < 0){
+        my_str("error while creating socket\n");
+        return 1;
+    }
+
+    options = 1;
+    if(setsockopt(pfd, SOL_SOCKET, SO_REUSEADDR, (const void*)&options, sizeof(int)) < 0){
+        my_str("error occurred with setsockopt\n");
+        return 1;
+    }
+
+    bzero((char *)&server, sizeof(server));
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons((unsigned short)port);
+    server.sin_family = AF_INET;
+
+    int x = bind(pfd, (struct sockaddr *)&server, sizeof(server));
+    if(x < 0){
+        my_str("Error while binding\n");
+        return 1;
+    }
+
+    my_str("Server is running...\n");
+
+    x = listen(pfd, 20);
+    if(x < 0){
+        my_str("Error while listening\n");
+        return 1;
+    }
+
+    fd_set r;
+
+    cli_len = sizeof(cli);
+    int users = 0;
+    int max_fd = 0;
+    struct s_node* head = (struct s_node*)malloc(sizeof(struct s_node));
+    head = NULL;
+    struct s_node* t;
+    struct client* u = (struct client*)malloc(sizeof(struct client));
+
+
+    //continuous run, only breaks on certain conditions
+    while(1){
+        FD_ZERO(&r);
+        FD_SET(0, &r);
+        FD_SET(pfd, &r);
+        t = head; //traversal node
+
+        //traverse list
+        while(t != NULL){
+            u = t->elem;
+            FD_SET(u->fd, &r);
+            if(u->fd > max_fd)
+                max_fd = u->fd;
+            t = t->next;
+        }
+
+        if(max_fd < pfd){
+            max_fd = pfd;
+        }
+
+        select(max_fd+1, &r, NULL, NULL, NULL);
+
+        if(FD_ISSET(pfd, &r)){
+            if((cfd = accept(pfd, (struct sockaddr *)&cli, &cli_len)) < 0){
+                my_str("Error accepting\n");
+                return 1;
+            }
+            for(int i = 0; i < users; i++){
+                u = elem_at(head, i);
+                if (cfd == u->fd)
+                    c = 1;
+
+            }
+            if(c == 0){
+                h = gethostbyaddr((const char *)&cli.sin_addr.s_addr, sizeof(cli.sin_addr.s_addr), AF_INET);
+                if (h == NULL){
+                    my_str("error with gethostbyaddr\n");
+                    return 1;
+                }
+                hostaddr = inet_ntoa(cli.sin_addr);
+                if(hostaddr == NULL){
+                    my_str("error with inet_ntoa\n");
+                    return 1;
+                }
+                my_str(h->h_name);
+                my_str(" has now joined the server!\n");
+                my_str("hostaddr: ");
+                my_str(hostaddr);
+                my_str("\n");
+
+                bzero(buf, 1024);
+
+                if ((n = read(cfd, buf, 100)) < 0){
+                    my_str("error reading socket.\n");
+                    return 1;
+                }
+
+                if(n > 0){
+                    char *usr = get_usr(buf);
+
+                    append(new_node(user(cfd, usr, h, hostaddr)), &head);
+
+                    users = count_s_nodes(head);
+
+                    char* init_msg = my_strconcat(usr, " is now on the chat!");
+
+                    my_str(my_strconcat(init_msg, "\n"));
+
+                    for (int i = 0; i < users; i++){
+                        u = elem_at(head, i);
+                        if ((n = write(u->fd, init_msg, my_strlen(init_msg))) < 0){
+                            my_str("Error writing to socket!\n");
+                            return 1;
+                        }
+                    }
+                }
+            }
+
+            c = 0;
+        }
+
+        users = count_s_nodes(head);
+        for(int i = 0; i < users; i++){
+            u = elem_at(head, i);
+            if(FD_ISSET(u->fd, &r)){
+                bzero(msg, 1024);
+                if ((n = read(u->fd, msg, 1024)) < 0){
+                    my_str("error while reading socket\n");
+                    return 1;
+                }
+                if (n == 0){
+                    char* name = u->usr;
+                    free(remove_node_at(&head, i));
+                    users-=1;
+
+                    char* exit_msg = my_strconcat(name, " has left the chat.");
+                    my_str(my_strconcat(exit_msg, "\n"));
+                    for(int i = 0; i < users; i++){
+                        u = elem_at(head, i);
+                        int msg_len = my_strlen(exit_msg);
+                        if((n = write(u->fd, exit_msg, msg_len)) < 0){
+                            my_str("error while writing\n");
+                            return 1;
+                        }
+                    }
+                } else if (msg[0] == '/'){
+                    int special = spec_options(&msg[0]);
+                    switch(special){
+                        case 1:
+                            {
+                            char* usr_msg = my_strconcat(u->usr, &msg[3]);
+                            my_str(my_strconcat(usr_msg, "\n"));
+                            for(int i = 0; i < users; i++){
+                                u = elem_at(head, i);
+                                if ((n = write(u->fd, usr_msg, my_strlen(usr_msg))) < 0){
+                                    my_str("Error while writing\n");
+                                    return 1;
+                                }
+                            }
+                            break;
+                            }
+                        case 2:
+                            {
+                            char* new_name = (char *)malloc(sizeof(char) * 1024);
+                            my_strcpy(new_name, &msg[6]);
+                            u->usr = new_name;
+                            char* show_msg = my_strconcat(u->usr, my_strconcat(" is now ", new_name));
+                            my_str(my_strconcat(show_msg, "\n"));
+                            for(int i = 0; i < users; i++){
+                                u = elem_at(head, i);
+                                if ((n = write(u->fd, show_msg, my_strlen(show_msg))) < 0){
+                                    my_str("error while writing!\n");
+                                    return 1;
+                                }
+                            }
+                            break;
+                            }
+                        case 3:
+                            {
+                            char* exit_msg = my_strconcat(u->usr, " has left the chat!");
+                            char* gib = "garbage";
+                            my_str(my_strconcat(exit_msg, "\n"));
+                            if((n = write(u->fd, gib, my_strlen(gib))) < 0){
+                                my_str("error while writing\n");
+                                return 1;
+                            }
+                            free(remove_node_at(&head, i));
+                            users-=1;
+                            for(int i = 0; i < users; i++){
+                                u = elem_at(head, i);
+                                if((n = write(u->fd, exit_msg, my_strlen(exit_msg))) < 0){
+                                    my_str("error while writing\n");
+                                    return 1;
+                                }
+                            }
+                            break;
+                            }
+                        default:
+                            {
+                            char* invalid_msg = "Valid commands: /nick <text>, /me <text>, /exit";
+                            my_str(invalid_msg);
+                            my_char('\n');
+                            if ((n = write(u->fd, invalid_msg, my_strlen(invalid_msg))) < 0) {
+                                my_str("error while writing\n");
+                                return 1;
+                            }
+                            break;
+                            }
+                    }
+                } else {
+                    char* usr_message = my_strconcat(my_strconcat(u->usr, ": "), msg);
+                    my_str(my_strconcat(usr_message, "\n"));
+                    for(int i = 0; i < users; i++){
+                        u = elem_at(head, i);
+                        if((n = write(u->fd, usr_message, my_strlen(usr_message))) < 0){
+                            my_str("error while writing!\n");
+                            return 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //cleanup for no memory leaks!
+    while(head != NULL){
+        u = head->elem;
+        close(u->fd);
+        head = head->next;
+        free(head->prev);
+    }
+
+    free(buf);
+    free(msg);
+    free(&head);
+    free(u);
+    return 0;
+}
